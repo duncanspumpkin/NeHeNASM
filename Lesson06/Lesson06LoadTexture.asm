bf79307760d0d9e21ad03c917b1f1465c03ddd59
Duncan Frost <duncans_pumpkin@hotmail.co.uk>
2013-04-05 17:08:24 +0100

    Updated all LoadTexture.asm files after code review

--- Lesson06/Lesson06LoadTexture.asm
+++ Lesson06/Lesson06LoadTexture.asm
@@ -1,240 +1,279 @@
+;*************************************
+;*  LoadTexture.asm by Duncan Frost  *
+;*            05/04/2013             *
+;*************************************
+
+;Exposes "LoadGLTextures" function - this will
+;load the texture specified in fileName variable
+
+;Exposes "texture" object - this is an array of
+;all of the loaded textures. It can be more than
+;one item due to different filters.
+
 %include "WIN32N.INC"
 %include "OPENGL32N.INC"
 %include "GLU32N.INC"
 
-extern CreateFileA
-extern ReadFile
-extern CloseHandle
-extern SetFilePointer
-extern GetProcessHeap
-extern HeapAlloc
-extern HeapFree
+extern LocalAlloc
+extern LocalFree
 extern glGenTextures
 extern glBindTexture
 extern glTexImage2D
 extern glTexParameteri
 extern glGetError
+extern gluBuild2DMipmaps
+extern LoadImageA
+extern GetObjectA
+extern SelectObject
+extern CreateCompatibleDC
+extern GetDIBits
+extern DeleteObject
+extern DeleteDC
+extern MessageBoxA
 
-import CreateFileA kernel32.dll
-import ReadFile kernel32.dll
-import SetFilePointer kernel32.dll
-import CloseHandle kernel32.dll
-import GetProcessHeap kernel32.dll
-import HeapAlloc kernel32.dll
-import HeapFree kernel32.dll
+
+import LocalAlloc kernel32.dll
+import LocalFree kernel32.dll
 import glGenTextures opengl32.dll
 import glBindTexture opengl32.dll
 import glTexImage2D opengl32.dll
 import glTexParameteri opengl32.dll
 import glGetError opengl32.dll
+import gluBuild2DMipmaps glu32.dll
+import LoadImageA User32.dll
+import MessageBoxA User32.dll
+import GetObjectA gdi32.dll
+import SelectObject gdi32.dll
+import CreateCompatibleDC gdi32.dll
+import GetDIBits gdi32.dll
+import DeleteObject gdi32.dll
+import DeleteDC gdi32.dll
 
 global LoadGLTextures
 global texture
 
 segment .code public use32 CLASS=CODE
 
-;Returns zero on fail
-;Parameters: fileName
-;Basically assumes file will be 24bpp
-ReadBitmap:
-.fileName equ 8
-.bitmapFileHdr equ BITMAPFILEHEADER_size
-.bitmapInfoHdr equ .bitmapFileHdr+BITMAPINFOHEADER_size
-.bytesRead equ .bitmapInfoHdr+6 
-;Note we have added 2 extra bytes as bitmapFileHdr is 2 bytes short of alignment
-.hFile equ .bytesRead+4
-.hHeap equ .hFile+4
-.noBytes equ .hHeap+4
-.ptrPixel equ .noBytes+4
-  ENTER .ptrPixel,0
+;********************************************
+;           LoadBitmap (string FileName)
+;
+;Loads the bitmap specified by FileName input
+;variable. Will convert format to 24bit a pixel
+;from whatever the loaded file was.
+;
+;Returns a pointer to the bitmap pixel data
+;The first 2 dwords contain width and length
+;Returns zero if failed
+; _______ ________ __________________________
+;| Width | Height | PixelData 24bit a pixel  |
+;|_______|________|__________________________|
+;********************************************
+
+LoadBitmap:
+ .FileName equ 8
+
+ ;This will contain information on the loaded bitmap
+ .ImgInfo equ BITMAP_size
+ 
+ ;This will be used to specify output format of loadbitmap
+ .BitmapInfo equ .ImgInfo+BITMAPINFOHEADER_size
+
+ ;Stores handle to bitmap
+ .hBitmap equ .BitmapInfo+4
+ 
+ ;Handle to temporary DC required for format changes
+ .hDC equ .hBitmap+4
+ 
+ ;Storage of output pointer
+ .DataPtr equ .hDC+4
+
+  enter .DataPtr,0
+
+  push dword LR_LOADFROMFILE|LR_CREATEDIBSECTION
   push dword 0
-  push dword FILE_ATTRIBUTE_NORMAL
-  push dword OPEN_EXISTING
   push dword 0
-  push dword FILE_SHARE_READ
-  push dword GENERIC_READ
-  push dword fileName;[ebp+.fileName]
-  call [CreateFileA]
-  mov [ebp-.hFile],eax
-
-  cmp eax,INVALID_HANDLE_VALUE
-  jne .CreateFileSuccess
-  xor eax,eax
-  jmp .ReadBitmapEnd
-
- .CreateFileSuccess:
-
+  push dword IMAGE_BITMAP
+  push dword [ebp+.FileName]
   push dword 0
-  lea ebx,[ebp-.bytesRead]
-  push ebx
-  push dword .bitmapInfoHdr ;Fills in file hdr and info hdr
-  lea ebx,[ebp-.bitmapInfoHdr]
-  push ebx
-  push eax
-  call [ReadFile]
-
-  cmp dword [ebp-.bytesRead],.bitmapInfoHdr
-  jne .ReadFail
+  call [LoadImageA]
+  mov [ebp-.hBitmap],eax
 
   sub eax,0
-  jnz .ReadSuccess
-
- .ReadFail: 
-  xor eax,eax
-  jmp .ReadBitmapEnd
-
- .ReadSuccess:
-  
-  ;We have loaded the file in reverse
-  ;so we need to remember to look at it back to front
-  
-  ;Reposition in file for pixel data
-  push dword FILE_BEGIN
+  jnz .LoadImageGood
+  push dword MB_OK|MB_ICONEXCLAMATION
+  push dword LDFAILT
+  push dword LDFAIL
   push dword 0
-  push dword [ebx+BITMAPFILEHEADER.bfOffBits] ;NOT CORRECT
-  push dword [ebp-.hFile]
-  call [SetFilePointer]
-
-  sub eax,0
-  jnz .SetFP
+  call [MessageBoxA]  
   xor eax,eax
-  jmp .ReadBitmapEnd
+  jmp .LoadBitmapEnd
 
- .SetFP:
-  ;((bitsperpix*width+31)/32)*4*height
-  lea ebx,[ebp-BITMAPINFOHEADER_size]
-  xor eax,eax
-  mov ax,[ebx+BITMAPINFOHEADER.biBitCount]
-  mov edi,[ebx+BITMAPINFOHEADER.biWidth]
-  mul edi
-  add eax,31
-  shr eax,5
-  shl eax,2
-  mov edi,[ebx+BITMAPINFOHEADER.biHeight]
-  mul edi ;number of bytes in for data.
-  mov ebx,eax
-  mov [ebp-.noBytes],eax
-  add ebx,8 ;Add space for x*y
-  
-  call [GetProcessHeap]
-  mov [ebp-.hHeap],eax
-  
+ .LoadImageGood:
+  ;Now that image has loaded correctly we
+  ;need to convert to prefered pixel format
+  ;i.e. 24bit a pixel
+
+  ;To convert we need some more info on this
+  ;bitmap such as height and width.
+  lea ebx,[ebp-.ImgInfo]
   push ebx
-  push HEAP_ZERO_MEMORY
+  push dword BITMAP_size
   push eax
-  call [HeapAlloc]
-  mov [ebp-.ptrPixel],eax
-
-  sub eax,0
-  jnz .HeapAlloced
-  xor eax,eax
-  jmp .ReadBitmapEnd
-
- .HeapAlloced:
-  lea ebx,[ebp-BITMAPINFOHEADER_size]
-  mov dword edi,[ebx+BITMAPINFOHEADER.biWidth]
-  mov [eax],edi 
-  add eax,4
-  mov dword edi,[ebx+BITMAPINFOHEADER.biHeight]
-  mov [eax],edi
-  add eax,4
+  call [GetObjectA]  ;Fill in the imgInfo
+  
+  ;Create a DC for our image format changes
   push dword 0
-  lea ebx,[ebp-.bytesRead]
-  push ebx
-  push dword [ebp-.noBytes]
+  call [CreateCompatibleDC]
+  mov dword [ebp-.hDC],eax
+  
+  sub eax,0
+  jz .LoadBitmapEnd
+
+  ;Select the bitmap onto the DC  
+  push dword [ebp-.hBitmap]
   push eax
-  push dword [ebp-.hFile]
-  call [ReadFile]
+  call [SelectObject]
   
-  push dword [ebp-.hFile]
-  call [CloseHandle] 
-  mov eax,[ebx]
-  cmp [ebp-.noBytes],eax
-  je .ReadBitSuccess
-  xor eax,eax
-  jmp .ReadBitmapEnd
+  ;Specify our new image format. The only important
+  ;part is the biBitCount being 24 bits.
+  lea ecx,[ebp-.BitmapInfo]
+  mov dword [ecx+BITMAPINFOHEADER.biSize],BITMAPINFOHEADER_size
+  mov dword eax,[ebx+BITMAP.bmWidth]
+  mov dword [ecx+BITMAPINFOHEADER.biWidth],eax
+  mov dword eax,[ebx+BITMAP.bmHeight]
+  mov dword [ecx+BITMAPINFOHEADER.biHeight],eax
+  mov word [ecx+BITMAPINFOHEADER.biPlanes],0x1
+  mov word [ecx+BITMAPINFOHEADER.biBitCount],24 ;Remember to change heap alloc as well
+  mov dword [ecx+BITMAPINFOHEADER.biCompression],0
+  mov dword [ecx+BITMAPINFOHEADER.biSizeImage],0
+  mov dword [ecx+BITMAPINFOHEADER.biXPelsPerMeter],0
+  mov dword [ecx+BITMAPINFOHEADER.biYPelsPerMeter],0
+  mov dword [ecx+BITMAPINFOHEADER.biClrUsed],0x0
+  mov dword [ecx+BITMAPINFOHEADER.biClrImportant],0x0
 
- .ReadBitSuccess:
-  ;Swap bytes so pixels in rgb format not bgr
-  mov eax,[ebp-.ptrPixel]
-  add eax,8 ;Jump over x/y dimensions
-  mov edx,eax
-  add edx,[ebp-.noBytes]
-
- .SwapLoop:
-  mov dword ecx,[eax]
-  mov ebx,ecx
-  bswap ebx
-  shr ebx,8
-  and ecx,0xff000000
-  or ebx,ecx
-  mov dword [eax],ebx
-  add eax,3
-  cmp eax,edx
-  jl .SwapLoop
+  ;Calculate the size of the output data. 
+  ;bitmap pixel data (width*height*3bytes) + width + height
+  mov dword ecx,[ebx+BITMAP.bmWidth]
+  mov dword eax,[ebx+BITMAP.bmHeight]
+  mul ecx
+  mov dword ecx,3
+  mul ecx ;3 bytes for each pixel
   
-  ;Add code to switch from bottom to top to top to bottom pixel rows
-  mov eax,[ebp-.ptrPixel]
+  ;Add on two dwords for width and height at start of data
+  add eax,8
+  mov dword ebx,eax
 
- .ReadBitmapEnd:
+  ;Find some clear memory
+  push ebx
+  push LMEM_ZEROINIT
+  call [LocalAlloc]
+  mov [ebp-.DataPtr],eax
+
+  ;Save our height & width
+  lea ebx,[ebp-.BitmapInfo]
+  mov dword ecx,[ebx+BITMAP.bmWidth]
+  mov [eax],ecx
+  mov dword ecx,[ebx+BITMAP.bmHeight]
+  mov [eax+4],ecx
+  add dword eax,8
+
+  ;Save our pixel data
+  push dword DIB_RGB_COLORS
+  push ebx
+  push eax 
+  push dword [eax-4] 
+  push dword 0
+  push dword [ebp-.hBitmap]
+  push dword [ebp-.hDC]
+  call [GetDIBits]
+  
+  ;Clean up
+  push dword [ebp-.hBitmap]
+  call [DeleteObject]
+
+  push dword [ebp-.hDC]
+  call [DeleteDC]
+
+  ;Present our final output
+  mov eax,[ebp-.DataPtr]  
+ .LoadBitmapEnd:
   leave
-ret 4
+retn 4
 
+;********************************************
+;           LoadGLTextures
+;
+;This function loads the bitmap specified in
+;local static variable "fileName" and converts
+;the bitmap into a GL texture.
+;You can then used the loaded GL texture with 
+;the global array "texture".
 ;Returns non zero on success
-LoadGLTextures:
-  push fileName
-  call ReadBitmap
-  mov ebx,eax
+;********************************************
 
-  sub eax,0
-  jz .LoadGLTexturesEnd
-  
+LoadGLTextures:
+  ;Load our bitmap file returns pointer to data
+  push dword fileName
+  call LoadBitmap
+
+  sub dword eax,0
+  jnz .LoadBitmapGood
+  push dword MB_OK|MB_ICONEXCLAMATION
+  push dword LDFAILT
+  push dword LDBITFAIL
+  push dword 0
+  call [MessageBoxA]
+  xor eax,eax
+  jmp .LoadGLTexturesEnd
+
+ .LoadBitmapGood:
+  mov ebx,eax
+  ;Jump over width & height
+  add dword ebx,8
+
+  ;We have 1 different filters we want
+  ;to show off so define 1 textures
   push dword texture
-  push dword 1  
+  push dword 1 
   call [glGenTextures]
   
-  call [glGetError]
-
   push dword [texture]
   push GL_TEXTURE_2D
   call [glBindTexture]
-  
-  call [glGetError]
-
-  add ebx,8
-  push ebx
-  push dword GL_UNSIGNED_BYTE
-  push dword GL_RGB;GL_BGR_EXT
-  push dword 0
-  sub ebx,4
-  push dword [ebx]
-  sub ebx,4
-  push dword [ebx]
-  push dword 3
-  push dword 0
-  push dword GL_TEXTURE_2D
-  call [glTexImage2D]
-
-  call [glGetError]
-
-  push dword GL_LINEAR
-  push dword GL_TEXTURE_MIN_FILTER
-  push dword GL_TEXTURE_2D
-  call [glTexParameteri]
 
   push dword GL_LINEAR
   push dword GL_TEXTURE_MAG_FILTER
   push dword GL_TEXTURE_2D
   call [glTexParameteri]  
 
-  call [glGetError]
+  push dword GL_LINEAR
+  push dword GL_TEXTURE_MIN_FILTER
+  push dword GL_TEXTURE_2D
+  call [glTexParameteri]  
+ 
+  ;Generate the texture from the bitmap  
+  push dword ebx
+  push dword GL_UNSIGNED_BYTE
+  ;Bitmaps are generated blue green red not
+  ;red green blue. Although this is an extension
+  ;in the GPU the format will be flipped back to
+  ;BGR so using this ext should in theory be
+  ;quicker (depending on openGL implementation)
+  push dword GL_BGR_EXT
+  push dword 0
+  push dword [ebx-4]
+  push dword [ebx-8]
+  ;3? I think this means 3 bytes - 24 bpp
+  push dword 3
+  push dword 0
+  push dword GL_TEXTURE_2D
+  call [glTexImage2D]
 
-  call [GetProcessHeap]
+  ;Go back to start of pointer (width + height)
+  ;so that we can free all of the pointer
+  sub ebx,8
   push ebx
-  push 0
-  push eax
-  call [HeapFree]
-  ;free heap
+  call [LocalFree]
 
   mov dword eax,1
 
@@ -242,8 +281,10 @@ LoadGLTextures:
 ret
 
 section .bss
-nBytes resd 1
 texture resd 1
 
 section .data use32
 fileName db "NeHe.bmp",0  
+LDFAIL db "Failed to load Image! Is file name incorrect?",0
+LDFAILT db "Load Fail",0
+LDBITFAIL db "Failed to load Bitmap!",0
--- Lesson10/Lesson10LoadTexture.asm
+++ Lesson10/Lesson10LoadTexture.asm
@@ -1,3 +1,15 @@
+;*************************************
+;*  LoadTexture.asm by Duncan Frost  *
+;*            05/04/2013             *
+;*************************************
+
+;Exposes "LoadGLTextures" function - this will
+;load the texture specified in fileName variable
+
+;Exposes "texture" object - this is an array of
+;all of the loaded textures. It can be more than
+;one item due to different filters.
+
 %include "WIN32N.INC"
 %include "OPENGL32N.INC"
 %include "GLU32N.INC"
@@ -17,6 +29,7 @@ extern CreateCompatibleDC
 extern GetDIBits
 extern DeleteObject
 extern DeleteDC
+extern MessageBoxA
 
 
 import LocalAlloc kernel32.dll
@@ -28,6 +41,7 @@ import glTexParameteri opengl32.dll
 import glGetError opengl32.dll
 import gluBuild2DMipmaps glu32.dll
 import LoadImageA User32.dll
+import MessageBoxA User32.dll
 import GetObjectA gdi32.dll
 import SelectObject gdi32.dll
 import CreateCompatibleDC gdi32.dll
@@ -40,16 +54,39 @@ global texture
 
 segment .code public use32 CLASS=CODE
 
-;Returns a pointer to the bitmap data
+;********************************************
+;           LoadBitmap (string FileName)
+;
+;Loads the bitmap specified by FileName input
+;variable. Will convert format to 24bit a pixel
+;from whatever the loaded file was.
+;
+;Returns a pointer to the bitmap pixel data
 ;The first 2 dwords contain width and length
+;Returns zero if failed
+; _______ ________ __________________________
+;| Width | Height | PixelData 24bit a pixel  |
+;|_______|________|__________________________|
+;********************************************
+
 LoadBitmap:
  .FileName equ 8
 
+ ;This will contain information on the loaded bitmap
  .ImgInfo equ BITMAP_size
+ 
+ ;This will be used to specify output format of loadbitmap
  .BitmapInfo equ .ImgInfo+BITMAPINFOHEADER_size
+
+ ;Stores handle to bitmap
  .hBitmap equ .BitmapInfo+4
+ 
+ ;Handle to temporary DC required for format changes
  .hDC equ .hBitmap+4
+ 
+ ;Storage of output pointer
  .DataPtr equ .hDC+4
+
   enter .DataPtr,0
 
   push dword LR_LOADFROMFILE|LR_CREATEDIBSECTION
@@ -62,26 +99,43 @@ LoadBitmap:
   mov [ebp-.hBitmap],eax
 
   sub eax,0
-  jz .LoadBitmapEnd
+  jnz .LoadImageGood
+  push dword MB_OK|MB_ICONEXCLAMATION
+  push dword LDFAILT
+  push dword LDFAIL
+  push dword 0
+  call [MessageBoxA]  
+  xor eax,eax
+  jmp .LoadBitmapEnd
 
+ .LoadImageGood:
+  ;Now that image has loaded correctly we
+  ;need to convert to prefered pixel format
+  ;i.e. 24bit a pixel
+
+  ;To convert we need some more info on this
+  ;bitmap such as height and width.
   lea ebx,[ebp-.ImgInfo]
   push ebx
   push dword BITMAP_size
   push eax
   call [GetObjectA]  ;Fill in the imgInfo
   
-
+  ;Create a DC for our image format changes
   push dword 0
   call [CreateCompatibleDC]
   mov dword [ebp-.hDC],eax
   
   sub eax,0
   jz .LoadBitmapEnd
-  
+
+  ;Select the bitmap onto the DC  
   push dword [ebp-.hBitmap]
   push eax
   call [SelectObject]
   
+  ;Specify our new image format. The only important
+  ;part is the biBitCount being 24 bits.
   lea ecx,[ebp-.BitmapInfo]
   mov dword [ecx+BITMAPINFOHEADER.biSize],BITMAPINFOHEADER_size
   mov dword eax,[ebx+BITMAP.bmWidth]
@@ -97,20 +151,25 @@ LoadBitmap:
   mov dword [ecx+BITMAPINFOHEADER.biClrUsed],0x0
   mov dword [ecx+BITMAPINFOHEADER.biClrImportant],0x0
 
+  ;Calculate the size of the output data. 
+  ;bitmap pixel data (width*height*3bytes) + width + height
   mov dword ecx,[ebx+BITMAP.bmWidth]
   mov dword eax,[ebx+BITMAP.bmHeight]
   mul ecx
   mov dword ecx,3
   mul ecx ;3 bytes for each pixel
-  add eax,8 ;2 dwords for width/height
+  
+  ;Add on two dwords for width and height at start of data
+  add eax,8
   mov dword ebx,eax
 
+  ;Find some clear memory
   push ebx
   push LMEM_ZEROINIT
   call [LocalAlloc]
   mov [ebp-.DataPtr],eax
 
-
+  ;Save our height & width
   lea ebx,[ebp-.BitmapInfo]
   mov dword ecx,[ebx+BITMAP.bmWidth]
   mov [eax],ecx
@@ -118,34 +177,62 @@ LoadBitmap:
   mov [eax+4],ecx
   add dword eax,8
 
+  ;Save our pixel data
   push dword DIB_RGB_COLORS
   push ebx
-  push eax
-  push dword [eax-4]
+  push eax 
+  push dword [eax-4] 
   push dword 0
   push dword [ebp-.hBitmap]
   push dword [ebp-.hDC]
   call [GetDIBits]
   
+  ;Clean up
   push dword [ebp-.hBitmap]
   call [DeleteObject]
 
   push dword [ebp-.hDC]
   call [DeleteDC]
 
+  ;Present our final output
   mov eax,[ebp-.DataPtr]  
  .LoadBitmapEnd:
   leave
 retn 4
 
+;********************************************
+;           LoadGLTextures
+;
+;This function loads the bitmap specified in
+;local static variable "fileName" and converts
+;the bitmap into a GL texture.
+;You can then used the loaded GL texture with 
+;the global array "texture".
 ;Returns non zero on success
-LoadGLTextures:
+;********************************************
 
+LoadGLTextures:
+  ;Load our bitmap file returns pointer to data
   push dword fileName
   call LoadBitmap
+
+  sub dword eax,0
+  jnz .LoadBitmapGood
+  push dword MB_OK|MB_ICONEXCLAMATION
+  push dword LDFAILT
+  push dword LDBITFAIL
+  push dword 0
+  call [MessageBoxA]
+  xor eax,eax
+  jmp .LoadGLTexturesEnd
+
+ .LoadBitmapGood:
   mov ebx,eax
+  ;Jump over width & height
   add dword ebx,8
 
+  ;We have 1 different filters we want
+  ;to show off so define 1 textures
   push dword texture
   push dword 1 
   call [glGenTextures]
@@ -153,7 +240,7 @@ LoadGLTextures:
   push dword [texture]
   push GL_TEXTURE_2D
   call [glBindTexture]
-  
+
   push dword GL_LINEAR
   push dword GL_TEXTURE_MAG_FILTER
   push dword GL_TEXTURE_2D
@@ -163,18 +250,27 @@ LoadGLTextures:
   push dword GL_TEXTURE_MIN_FILTER
   push dword GL_TEXTURE_2D
   call [glTexParameteri]  
-
+ 
+  ;Generate the texture from the bitmap  
   push dword ebx
   push dword GL_UNSIGNED_BYTE
+  ;Bitmaps are generated blue green red not
+  ;red green blue. Although this is an extension
+  ;in the GPU the format will be flipped back to
+  ;BGR so using this ext should in theory be
+  ;quicker (depending on openGL implementation)
   push dword GL_BGR_EXT
   push dword 0
   push dword [ebx-4]
   push dword [ebx-8]
+  ;3? I think this means 3 bytes - 24 bpp
   push dword 3
   push dword 0
   push dword GL_TEXTURE_2D
   call [glTexImage2D]
 
+  ;Go back to start of pointer (width + height)
+  ;so that we can free all of the pointer
   sub ebx,8
   push ebx
   call [LocalFree]
@@ -189,3 +285,6 @@ texture resd 1
 
 section .data use32
 fileName db "Mud.bmp",0  
+LDFAIL db "Failed to load Image! Is file name incorrect?",0
+LDFAILT db "Load Fail",0
+LDBITFAIL db "Failed to load Bitmap!",0
